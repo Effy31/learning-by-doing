dafd0786ac1f1eb2529dbec91da754fd37da096c
Effy31 <64260496+Effy31@users.noreply.github.com>
2020-04-24 20:43:00 +0200

    Create pythagoras.py
    initial

---
+++ pythagoras.py
@@ -0,0 +1,12 @@
+# Hypotenuse berechnen
+# a**2 + b**2 = c**2
+
+import math
+
+print("Berechne die Hypotenuse:")
+a=int(input("Geben Sie a an:"))
+b=int(input("Geben Sie b an:"))
+
+c=((a**2)+(b**2))**0.5
+
+print("Hypothenuse c:"+str(c))
